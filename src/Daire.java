414f62521c50cfd02a0ab5ab01b2ff7662a90e2d
berkbahadir34 <berkbahadir34@gmail.com>
2020-10-10 19:39:53 +0300

    Abstract Classlar Kullanılarak Geometrik Şekil Hesaplama Programı

---
+++ src/Daire.java
@@ -0,0 +1,14 @@
+public class Daire extends Sekil {
+private int yaricap;
+
+    public Daire(String isim, int yaricap) {
+        super(isim);
+        this.yaricap = yaricap;
+    }
+
+    @Override
+    public void alanHesapla() {
+        System.out.println(getIsim()+" 'ın alanı : "+(Math.PI*yaricap*yaricap));
+
+    }
+}
